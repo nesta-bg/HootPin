eb66a37267152fa7407eeb92eaab472cb0ed887f
nenadnesta <nenadnesta@gmail.com>
2019-05-20 14:14:51 +0200

    Create Add A Hoot Form.

---
+++ HootPin/Controllers/HootsController.cs
@@ -0,0 +1,28 @@
+﻿using HootPin.Models;
+using HootPin.ViewModels;
+using System.Web.Mvc;
+using System.Linq;
+
+namespace HootPin.Controllers
+{
+    public class HootsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public HootsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        // GET: Hoots
+        public ActionResult Create()
+        {
+            var viewModel = new HootFormViewModel
+            {
+                Genres = _context.Genres.ToList()
+            };
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ HootPin/ViewModels/HootFormViewModel.cs
@@ -0,0 +1,14 @@
+﻿using HootPin.Models;
+using System.Collections.Generic;
+
+namespace HootPin.ViewModels
+{
+    public class HootFormViewModel
+    {
+        public string Venue { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public int Genre { get; set; }
+        public IEnumerable<Genre> Genres { get; set; }
+    }
+}
