acfa90ad9f1a2a0f0a49a3d4faafa67a980b4ecf
nenadnesta <nenadnesta@gmail.com>
2019-05-20 13:37:56 +0200

    Create Hoot And Genre Models.

--- HootPin/Models/IdentityModels.cs
+++ HootPin/Models/IdentityModels.cs
@@ -20,6 +20,9 @@ namespace HootPin.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public DbSet<Hoot> Hoots { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
