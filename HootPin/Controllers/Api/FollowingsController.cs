13c1c4fac951137776d45222d29725d7d89815c8
nenadnesta <nenadnesta@gmail.com>
2019-05-22 14:29:01 +0200

    Following System.

---
+++ HootPin/Controllers/Api/FollowingsController.cs
@@ -0,0 +1,38 @@
+﻿using HootPin.Dtos;
+using HootPin.Models;
+using Microsoft.AspNet.Identity;
+using System.Linq;
+using System.Web.Http;
+
+namespace HootPin.Controllers.Api
+{
+    public class FollowingsController : ApiController
+    {
+        private readonly ApplicationDbContext _context;
+
+        public FollowingsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        public IHttpActionResult Follow(FollowingDto dto)
+        {
+            var userId = User.Identity.GetUserId();
+
+            if (_context.Followings.Any(f => f.FollowerId == userId && f.FolloweeId == dto.FolloweeId))
+                return BadRequest("Following already exists.");
+
+            var following = new Following
+            {
+                FollowerId = userId,
+                FolloweeId = dto.FolloweeId
+            };
+
+            _context.Followings.Add(following);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
